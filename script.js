204f19072c6c29b1e8f6e43ecf21bc712530a0f1
Your Name <s.insamam787@gmail.com>
2023-03-21 19:07:39 +0530

    function js

---
+++ script.js
@@ -0,0 +1,25 @@
+//function expression
+
+const add = function (a,b){
+    return a+b;
+}
+// function
+
+function add1 (a, b)
+{
+    return a+b;
+}
+
+// Arrow function
+
+const add2 = (a, b) => 
+{
+    return a+b;
+}
+
+console.log(add(5,4))
+console.log(add)
+console.log(add1(6,7))
+console.log(add2(7,7))
+
+
